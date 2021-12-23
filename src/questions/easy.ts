aad994e9d8e39ca3def46c319b4b467789998a21
David Chavez <depch47@gmail.com>
2021-12-23 16:29:42 -0500

    setting up

---
+++ src/questions/easy.ts
@@ -0,0 +1,9 @@
+import { Question } from '../interfaces/question'
+
+export const easy: Question[] = [
+  {
+    question: "What's the biggest animal in the world?",
+    answers: ['The blue whale', 'Tiger', 'Elephant', 'Dodo'],
+    correctAnswer: 'The blue whale',
+  },
+]
